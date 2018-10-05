d3008c798177aabe011fcb5905de78765b86da0b
tzheng24 <tzheng24@wisc.edu>
2018-10-04 23:49:50 -0500

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,8 +1,11 @@
 SELECT
-(SELECT COUNT(*) UserID FROM Seller WHERE Location = "New York")
+(SELECT COUNT(*) UserID FROM Item
+WHERE Location = "New York")
 +
-(SELECT COUNT(*) UserID FROM Bidder WHERE Location = "New York")
+(SELECT COUNT(*) UserID FROM Bidder
+WHERE Location = "New York")
 -
-(SELECT COUNT(*) FROM Seller S, Bidder B
-WHERE S.UserID = B.UserID
-AND Location = "New York");
+(SELECT COUNT(*) FROM Item I, Bidder B
+WHERE I.Location = "New York"
+AND B.Location = "New York"
+AND I.UserID = B.UserID);
